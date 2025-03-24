57423bff67d425e70856bd397b62d4cb2e075208
Le_Petit_C <2729362136@qq.com>
2025-03-24 14:05:20 +0800

    初始配置

--- src/client/java/lpcmcmt/ExampleModClient.java
+++ src/client/java/lpcmcmt/ExampleModClient.java
@@ -1,4 +1,4 @@
-package com.example;
+package lpcmcmt;
 
 import net.fabricmc.api.ClientModInitializer;
 
---
+++ src/main/java/lpcmcmt/ExampleMod.java
@@ -0,0 +1,17 @@
+package lpcmcmt;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ExampleMod implements ModInitializer {
+	public static final String MOD_ID = "lpcmcmt";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
